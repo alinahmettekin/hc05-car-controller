87a11572a06dd087f41d90c10cba0fd2501ec8de
alinahmettekin <alinahmettekin@gmail.com>
2024-08-03 14:57:40 +0300

    screen updates

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,21 +1,6 @@
+import 'package:car_control_app/screens/control_screen.dart';
 import 'package:flutter/material.dart';
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Uzaktan Araç Kontrol',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: const Color(0xFF1E1E1E),
-      ),
-      home: const HomePage(),
-    );
-  }
-}
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -26,9 +11,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
-  List<String> devices = [];
-  bool isScanning = false;
+  final FlutterBluetoothSerial _bluetooth = FlutterBluetoothSerial.instance;
+  final List<BluetoothDevice> _devices = [];
+  late bool _isScanning = false;
   late AnimationController _animationController;
+  BluetoothConnection? connection;
 
   @override
   void initState() {
@@ -37,6 +24,14 @@ class _HomePageState extends State<HomePage>
       vsync: this,
       duration: const Duration(seconds: 2),
     )..repeat();
+    _checkBluetoothStatus();
+  }
+
+  void _checkBluetoothStatus() async {
+    bool isEnabled = await _bluetooth.isEnabled ?? false;
+    if (!isEnabled) {
+      await _bluetooth.requestEnable();
+    }
   }
 
   @override
@@ -45,24 +40,82 @@ class _HomePageState extends State<HomePage>
     super.dispose();
   }
 
-  void scanDevices() {
+  void scanDevices() async {
     setState(() {
-      isScanning = true;
+      _isScanning = true;
+      _devices.clear();
     });
-    // Bluetooth tarama işlevi buraya eklenecek
-    Future.delayed(const Duration(seconds: 3), () {
-      setState(() {
-        devices = ['HC-05 Araç Kontrolü', 'Arduino Bluetooth Modülü'];
-        isScanning = false;
+
+    try {
+      _bluetooth.startDiscovery().listen((r) {
+        setState(() {
+          final existingIndex = _devices
+              .indexWhere((element) => element.address == r.device.address);
+          if (existingIndex >= 0) {
+            _devices[existingIndex] = r.device;
+          } else {
+            _devices.add(r.device);
+          }
+        });
+      }).onDone(() {
+        setState(() {
+          _isScanning = false;
+        });
       });
-    });
+    } catch (ex) {
+      print('Error scanning Bluetooth devices: $ex');
+      setState(() {
+        _isScanning = false;
+      });
+    }
+  }
+
+  void _connectToDevice(BluetoothDevice device) async {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Bağlanılıyor'),
+          content: Row(
+            children: [
+              const CircularProgressIndicator(),
+              const SizedBox(width: 20),
+              Text('${device.name} bağlanılıyor...'),
+            ],
+          ),
+        );
+      },
+    );
+
+    try {
+      connection = await BluetoothConnection.toAddress(device.address);
+      print('Connected to the device');
+
+      Navigator.of(context).pop(); // Dialog'u kapat
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) =>
+              ControlScreen(device: device, connection: connection!),
+        ),
+      );
+    } catch (exception) {
+      print('Cannot connect, exception occurred: $exception');
+      Navigator.of(context).pop(); // Dialog'u kapat
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Bağlantı başarısız: $exception')),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Uzaktan Araç Kontrol'),
+        title: const Text(
+          'Uzaktan Araç Kontrol',
+          style: TextStyle(fontSize: 17),
+        ),
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
@@ -73,14 +126,8 @@ class _HomePageState extends State<HomePage>
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               const SizedBox(height: 20),
-              Text(
-                'Bluetooth Cihazları',
-                style: Theme.of(context).textTheme.headlineSmall,
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 40),
               ElevatedButton(
-                onPressed: isScanning ? null : scanDevices,
+                onPressed: _isScanning ? null : scanDevices,
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.blue,
                   shape: RoundedRectangleBorder(
@@ -89,7 +136,7 @@ class _HomePageState extends State<HomePage>
                 ),
                 child: Padding(
                   padding: const EdgeInsets.symmetric(vertical: 12.0),
-                  child: isScanning
+                  child: _isScanning
                       ? Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
@@ -112,12 +159,12 @@ class _HomePageState extends State<HomePage>
               ),
               const SizedBox(height: 30),
               Expanded(
-                child: devices.isEmpty
+                child: _devices.isEmpty
                     ? const Center(
                         child: Text('Henüz cihaz bulunamadı.'),
                       )
                     : ListView.builder(
-                        itemCount: devices.length,
+                        itemCount: _devices.length,
                         itemBuilder: (context, index) {
                           return Card(
                             elevation: 4,
@@ -126,33 +173,11 @@ class _HomePageState extends State<HomePage>
                             child: ListTile(
                               leading: const Icon(Icons.bluetooth,
                                   color: Colors.blue),
-                              title: Text(devices[index]),
+                              title: Text(
+                                  _devices[index].name ?? 'Bilinmeyen Cihaz'),
+                              subtitle: Text(_devices[index].address),
                               trailing: const Icon(Icons.arrow_forward_ios),
-                              onTap: () {
-                                // Bağlantı simülasyonu
-                                showDialog(
-                                  context: context,
-                                  barrierDismissible: false,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      title: const Text('Bağlanılıyor'),
-                                      content: Row(
-                                        children: [
-                                          const CircularProgressIndicator(),
-                                          const SizedBox(width: 20),
-                                          Text(
-                                              '${devices[index]} bağlanılıyor...'),
-                                        ],
-                                      ),
-                                    );
-                                  },
-                                );
-
-                                // 2 saniye sonra bağlantı başarılı varsayalım ve kontrol ekranına geçiş yapalım
-                                Future.delayed(const Duration(seconds: 2), () {
-                                  Navigator.of(context).pop(); // Dialog'u kapat
-                                });
-                              },
+                              onTap: () => _connectToDevice(_devices[index]),
                             ),
                           );
                         },
